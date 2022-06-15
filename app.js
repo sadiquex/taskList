8995ca249842d08d4b78ceda08c93eef3b37c3f1
sadiquex <abubakasaddik1@gmail.com>
2022-06-15 20:14:03 +0000

    added filter and remove tasks buttons

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners(){
     // add task event
     form.addEventListener('submit', addTask);
+    // remove task event
+    taskList.addEventListener('click', removeTask);
+    // clear task event
+    clearBtn.addEventListener('click', clearTasks)
+    // filter tasks event
+    filter.addEventListener('keyup', filterTasks)
 }
 
 // add task
@@ -42,4 +48,37 @@ function addTask(e){
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// remove task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains
+        ('delete-item')){
+            if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+            }
+        }
+}
+
+// clear tasks
+function clearTasks(){
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+    
+}
+
+// filter tasks
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach
+    (function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+    });
 }
